2ec25011296f30da37266b9a9c041683ede18f1b
nuwandavek <vivekaithal44@gmail.com>
2016-02-21 08:37:04 +0530

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		clean: ['dist'],
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					src: ['index.html','jsmain.js','cssmain.css','dep.html'],
+					dest: 'dist/'
+				}]
+			}
+		},
+				
+		'gh-pages': {
+			options: {
+				base: 'dist'
+			},
+			src: ['**']
+		}
+	});
+
+    grunt.registerTask('build', ['clean', 'copy:dist', 'htmlmin']);
+
+	require('load-grunt-tasks')(grunt);
+};
